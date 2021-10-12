6066f0125476ff003b65deebf4bc8bc261254214
Emeline <70700006+emelxne@users.noreply.github.com>
2021-10-12 22:44:32 +0200

    maj login et import firebase

---
+++ efreix/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+const app = firebase.initializeApp({
+    apiKey: process.env.REACT_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_FIREBASE_APPID,
+    measurementId: process.env.REACT_FIREBASE_MEASUREMENT_ID
+})
+
+export const auth = app.auth()
+export default app
